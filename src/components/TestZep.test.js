5228e7175e9f57bfa041db7d86d70b8ae6bbf9ba
leobauza <leonardobauza@gmail.com>
2018-08-12 15:49:59 -0400

    add simple test for TestZep component

--- src/components/TestZep.test.js
+++ src/components/TestZep.test.js
@@ -4,8 +4,7 @@ import TestZep from './TestZep'
 import Web3 from 'web3'
 
 it('renders without crashing', () => {
-  // @TODO - [ ] Change TestZep to use web3.eth.Contract so it's easier to test
-  // const web3 = new Web3()
-  // shallow(<TestZep web3={web3} />)
-  return undefined
+  // @TODO - [x] Change TestZep to use web3.eth.Contract so it's easier to test
+  const web3 = new Web3()
+  shallow(<TestZep web3={web3} />)
 })
